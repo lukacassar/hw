22b81215d45f4faf115b7f5ced74c61a1fcfe8d8
lukacassar <luka.cassar.24@um.edu.mt>
2025-05-09 12:26:52 +0200

    h

--- Python/DSA4.py
+++ Python/DSA4.py
@@ -1,11 +1,11 @@
 import random
 arr = []
-for i in range(10):
-    arr.append(random.randint(0,20))
+for i in range(50):
+    arr.append(random.randint(1,1024))
 
 combinations = {}
 
-# Storing ALL UNIQUE COMBINATIONS
+# Storing ALL UNIQUE COMBINATIONS as dictionary with array values
 for i in range(len(arr)):
     for j in range(i+1, len(arr)):
         a = arr[i]
@@ -14,10 +14,23 @@ for i in range(len(arr)):
 
         if product not in combinations.keys():
             combinations[product] = []
-        combinations[product].append((a,b))
+        if a>b: # Storing as a sorted tuple to avoid duplicates, easier to work with also.
+            a,b = b,a
+        if (a,b) not in combinations[product]:
+            combinations[product].append((a,b))
 
-pairs = {}
 
-
-
+for key in combinations.keys():
+    ptr = combinations.get(key)
+    if len(ptr) == 2:
+        print(f"Product {key} has combinations: {ptr}")
+    elif len(ptr) > 2:
+        print(f"Product {key} has the following combinations")
+        for i in range(len(ptr)):
+            for j in range(i+1, len(ptr)):
+                a = ptr[i]
+                b = ptr[j]
+                print(f"{a} and {b}")
+                
+                
 
